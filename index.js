0af4e89196b0b9fa751c10bfb178003a53b6d6c7
luzybry94 <luzybry94@gmail.com>
2021-02-03 16:56:21 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  const time = parseInt(string.split(":")[0]);
+  if (time < 12) {
+    return "Good Morning";
+  } else if (time < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string;
+}
